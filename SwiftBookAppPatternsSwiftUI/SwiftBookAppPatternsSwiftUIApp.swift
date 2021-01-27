04b3b1470d4d1892023e96ab02b75e23bff4fed7
Volodya <volodyavolga@protonmail.ch>
2021-01-27 12:26:39 +0300

    Initial Commit

---
+++ SwiftBookAppPatternsSwiftUI/SwiftBookAppPatternsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBookAppPatternsSwiftUIApp.swift
+//  SwiftBookAppPatternsSwiftUI
+//
+//  Created by Vladimir Stepanchikov on 27.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBookAppPatternsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
